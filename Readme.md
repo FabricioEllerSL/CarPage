6e24d826cdd0586f07f2ea11786eacf6126fcb63
Fabricio Eller <fabricioellersl@gmail.com>
2023-10-22 16:50:20 -0300

    Primeiro commit/geral

---
+++ Readme.md
@@ -0,0 +1,2 @@
+Esse projeto é um pequeno site sobre carros feito por um super fã do mundo
+automobilistico
---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// Função para abrir a barra de navegação lateral
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+// Função para fechar a barra de navegação lateral
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
