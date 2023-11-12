c0fd751f90f2ad8a6cedc174e4f13286cf949748
Fabricio Eller <fabricioellersl@gmail.com>
2023-11-12 20:23:46 -0300

    modais proprios hatch/sedan

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,40 @@ const submitButton = document.querySelector(".submitFormButton");
 submitButton.addEventListener("click", (e) => {
     e.preventDefault();
 })
+
+// Modal
+
+const saibaMaisHatch = document.querySelector(".more-button.hatch");
+const itemAbrir = document.querySelector(".category-container.hatch")
+const fechar = document.querySelector(".close-modal")
+
+
+
+
+saibaMaisHatch.addEventListener("click", (e) => {
+    e.preventDefault();
+    itemAbrir.classList.remove("hidden");
+
+})
+
+fechar.addEventListener("click", (e) => {
+    e.preventDefault();
+    itemAbrir.classList.add("hidden");
+
+})
+
+const saibaMaisSedan = document.querySelector(".more-button.sedan");
+const itemAbrir2 = document.querySelector(".category-container.sedan")
+const fechar2 = document.querySelector(".close-modal2")
+
+saibaMaisSedan.addEventListener("click", (e) => {
+    e.preventDefault();
+    itemAbrir2.classList.remove("hidden");
+
+})
+
+fechar2.addEventListener("click", (e) => {
+    e.preventDefault();
+    itemAbrir2.classList.add("hidden");
+
+})
